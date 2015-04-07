aabbb6c162ba744fa55fb39859ed1dc5e53028db
Oleg Semery <semery@cvisionlab.com>
2015-04-07 07:54:11 +0300

    Rename solution. Add httpclient project

---
+++ httpserver/httpclient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace httpclient
+{
+    class Program
+    {
+        static async void DownloadPageAsync()
+        {
+            // ... Target page.
+            string page = "http://en.wikipedia.org/";
+
+            // ... Use HttpClient.
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(page))
+            using (HttpContent content = response.Content)
+            {
+                // ... Read the string.
+                string result = await content.ReadAsStringAsync();
+
+                // ... Display the result.
+                if (result != null &&
+                result.Length >= 50)
+                {
+                    Console.WriteLine(result.Substring(0, 50) + "...");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task t = new Task(DownloadPageAsync);
+            t.Start();
+            Console.WriteLine("Downloading page...");
+            Console.ReadLine();
+        }
+    }
+}
