7d32e6a8d2d1147fcfb53830e4e4b2891b868fcf
Oleg Semery <semery@cvisionlab.com>
2015-04-07 07:47:27 +0300

    The second commit, add project httpserver

---
+++ httpserver/httpserver/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+
+namespace httpserver
+{
+    class Program
+    {
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now);
+        }
+
+        static void Main(string[] args)
+        {
+            WebServer ws = new WebServer(SendResponse, "http://localhost:8080/test/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+    }
+}
